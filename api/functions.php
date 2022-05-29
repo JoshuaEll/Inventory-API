43d721b4669ecccc08cc9d237fcc0bfae0d96d87
Joshua Ellis <71731580+JoshuaEll@users.noreply.github.com>
2022-05-29 02:17:09 -0500

    Update functions.php
    Got rid of un and pw information

--- api/functions.php
+++ api/functions.php
@@ -2,8 +2,8 @@
 
 function db_iconnect($dbName)
 {
-	$un = "webuser";
-	$pw = "gG6SLzdskA2IrbKs";
+	$un = "Enter the user name here";
+	$pw = "Enter the password here";
 	$db = $dbName;
 	$hostname = "localhost";
 	$dblink = new mysqli($hostname, $un, $pw, $db);
